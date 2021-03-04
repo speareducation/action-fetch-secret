c6c1af45501f9c858199d4e5e14de85106853ee2
Jason Wright <jwright@speareducation.com>
2021-03-04 10:26:59 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Spear Education Fetch Secret JS Action
+*Note: This action is designed to be compatible within Spear's ecosystem.
+
+Fetches a secret from AWS Secrets Manager and installs it at the specified location in .env format
+
+## Inputs
+
+### `secretPath`
+**Required** The name of the secret
+
+### `targetFile`
+**Required** The filename for the secret
+
+## Example usage
+```
+- id: fetch-secret
+  name: Fetch Secret
+  uses: speareducation/action-fetch-secret@main
+  env:
+    AWS_REGION: us-east-1
+    AWS_ACCESS_KEY_ID: ${{ secrets.SPEAR_AWS_ACCESS_KEY_ID }}
+    AWS_SECRET_ACCESS_KEY: ${{ secrets.SPEAR_AWS_SECRET_ACCESS_KEY }}
+  with:
+    secretPath: path/to/the/secret
+    targetFile: .env
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const { SecretsManager } = require('aws-sdk');
+const core = require('@actions/core');
+const fs = require('fs');
+
+const secretsManager = new SecretsManager();
+const SecretId = core.getInput('secretPath');
+const targetFile = core.getInput('targetFile');
+
+function createKeyValue(key, value) {
+    return value ? `${key}=${value}` : key; // [production] support
+}
+
+async function handle() {
+    console.log(`Fetching ${secretPath}`);
+    const response = await secretsManager.getSecretValue({ SecretId }).promise();
+    console.log(`Retrieved version ${secretData.VersionId}`);
+
+    const secretData = JSON.parse(response.SecretString);
+    
+    const secretPayload = Object.keys(secretData)
+        .map(key => createKeyValue(key, secretData[key]))
+        .join('\n');
+
+    console.log(`Writing secret to ${targetFile}`);
+    fs.writeFileSync(targetFile, secretPayload);
+}
+
+handle();
